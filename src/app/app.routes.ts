f8e6fb56dc4e821403a1b02312233ffa02c96faa
TPatel2909 <TPatel@datacentrix.co.za>
2025-01-14 15:47:53 +0200

    updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Login',
+  },
+  {
+    path: 'Tasks',
+    component: TasksComponent,
+    title: 'Tasks',
+  },
+];
