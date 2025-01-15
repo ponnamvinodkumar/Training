dc6f1de68532f3e9aeb484758cc3610713f8ca9e
TPatel2909 <TPatel@datacentrix.co.za>
2025-01-15 15:10:55 +0200

    Updated UI

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-tasks',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-
-}
+ 
+    isSidebarMinimized = true;
+  
+    toggleSidebar() {
+      this.isSidebarMinimized = !this.isSidebarMinimized;
+    }
+  }
+  
