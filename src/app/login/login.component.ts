49b5fac1d8119a583da43f60b6622cd297b87b02
Samukelo Msane <122905309+SamukeloThedeveloper@users.noreply.github.com>
2025-01-20 10:33:25 +0000

    getting latest changes before pushing.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+  email: string = '';
+  confirmPassword: string = '';
+  errorMessage: string = '';
+  isRegistering: boolean = false;  //flag, reg or login
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  register(): void {
+    if(this.password !== this.confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+      return;
+    }
+
+    const success = this.authService.register(this.username, this.password);
+
+    if(success) {
+      alert('Registration successful');
+      this.switchToLogin();
+    }else{
+      this.errorMessage = 'User already exists';
+    }
+  }
+
+  login(): void {
+    const success = this.authService.login(this.username, this.password);
+    if(success) {
+      this.authService.setCurrentUser(this.username);
+      this.router.navigate(['/dashboard']);
+    }else{
+      this.errorMessage = 'Invalid credentials';
+    }
+  }
+
+  switchToRegister(): void{
+    this.isRegistering = true;
+    this.errorMessage = '';   //reseting error message
+  }
+
+  switchToLogin(): void {
+    this.isRegistering = false;
+    this.errorMessage = '';
+  }
 }
