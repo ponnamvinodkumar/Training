fbb1738401283356877c01efdd35f1e68b69f05d
Tristan-Steed <tsteed861@gmail.com>
2025-01-22 13:35:48 +0200

    Initial build for my time entries screen

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+  ,imports: [RouterLink,CommonModule]
+})
+export class SidebarComponent {
+  isSidebarMinimized = true;
+
+  toggleSidebar() {
+    this.isSidebarMinimized = !this.isSidebarMinimized;
+  }
+}
